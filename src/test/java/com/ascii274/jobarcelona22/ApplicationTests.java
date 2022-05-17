90e3bad3993740c182384faf56b3f55e65a370fb
Joel Cabatay <ascii274@gmail.com>
2022-05-18 00:54:53 +0200

    Smoke test

--- src/test/java/com/ascii274/jobarcelona22/ApplicationTests.java
+++ src/test/java/com/ascii274/jobarcelona22/ApplicationTests.java
@@ -1,13 +1,21 @@
 package com.ascii274.jobarcelona22;
 
+import com.ascii274.jobarcelona22.controller.UserController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class ApplicationTests {
 
+	@Autowired
+	private UserController userController;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception{
+		assertThat(userController).isNotNull();
 	}
 
 }
